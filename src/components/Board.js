4e68ef833fdf71d36f9c97c9e27430cb82e17f23
Lorena <lorena.perez@gmail.com>
2021-07-17 15:05:06 +0200

    add images and array

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
+import { images } from "./import";
 import "../stylesheets/Board.css";
 
 function Board() {
   const [cards, setCards] = useState([1, 2]);
+
+  useEffect(() => {
+    setCards(images);
+  }, []);
   //const listCards = cards.map((card) => <span>{card}</span>);
   return (
     <div className="board">
---
+++ src/components/import.js
@@ -0,0 +1,94 @@
+import src1 from "../images/bdeo.png";
+import src2 from "../images/carrots-lab.jpeg";
+import src3 from "../images/emergya.png";
+import src4 from "../images/factorial.png";
+import src5 from "../images/glovo.png";
+import src6 from "../images/innocv.png";
+import src7 from "../images/Opentrends_.png";
+import src8 from "../images/sopra.png";
+import src9 from "../images/UGROUND.png";
+import src10 from "../images/wefox.jpg";
+
+const images = [
+  {
+    src: src1,
+    company: "bdeo",
+  },
+  {
+    src: src1,
+    company: "bdeo",
+  },
+  {
+    src: src2,
+    company: "carrontslab",
+  },
+  {
+    src: src2,
+    company: "carrontslab",
+  },
+  {
+    src: src3,
+    company: "emergya",
+  },
+  {
+    src: src3,
+    company: "emergya",
+  },
+  {
+    src: src4,
+    company: "factorial",
+  },
+  {
+    src: src4,
+    company: "factorial",
+  },
+  {
+    src: src5,
+    company: "glovo",
+  },
+  {
+    src: src5,
+    company: "glovo",
+  },
+  {
+    src: src6,
+    company: "innocv",
+  },
+  {
+    src: src6,
+    company: "innocv",
+  },
+  {
+    src: src7,
+    company: "opentrends",
+  },
+  {
+    src: src7,
+    company: "opentrends",
+  },
+  {
+    src: src8,
+    company: "sopra",
+  },
+  {
+    src: src8,
+    company: "sopra",
+  },
+  {
+    src: src9,
+    company: "uground",
+  },
+  {
+    src: src9,
+    company: "uground",
+  },
+  {
+    src: src10,
+    company: "wefox",
+  },
+  {
+    src: src10,
+    company: "wefox",
+  },
+];
+export { images };
