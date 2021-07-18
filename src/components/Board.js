bc8da6f439095d36312b5f05fd74400af19a1d31
Lorena <lorena.perez@gmail.com>
2021-07-18 10:02:41 +0200

    add shuffle

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,7 +5,17 @@ import { images } from "./import";
 function Board() {
   const [cards, setCards] = useState([1, 2]);
 
+  const shuffleArray = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  };
+
   useEffect(() => {
+    shuffleArray(images);
     setCards(images);
   }, []);
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,22 @@ import backFace from "../images/question.png";
 
 function Card({ name, number, frontFace }) {
   const [isFlipped, setIsFlipped] = useState(false);
+  const handleClick = () => setIsFlipped(!isFlipped);
   return (
     <div className="card">
       <ReactCardFlip isFlipped={isFlipped}>
-        <img className="card-image" src={backFace} alt="backface" />
-        <img className="card-image" src={frontFace} alt="frontFace" />
+        <img
+          className="card-image"
+          src={backFace}
+          alt="backface"
+          onClick={handleClick}
+        />
+        <img
+          className="card-image"
+          src={frontFace}
+          alt="frontFace"
+          onClick={handleClick}
+        />
       </ReactCardFlip>
     </div>
   );
